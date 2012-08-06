0b7f84f817677085618ce01886fe9409da734af7
Bruno Gurgel <bgoliveira@sp.r7.com>
2012-08-06 17:30:37 -0300

    Treaded

--- src/sbin/rlogix.rb
+++ src/sbin/rlogix.rb
@@ -1,14 +1,12 @@
 #!/usr/bin/ruby
 
 require "yaml"
-require "syslog"
 require "bunny"
-require "socket"
-require "json"
+require "thread"
 
 $config_file = "/etc/rlogix/rlogix.conf"
-$HOST = Socket.gethostname 
 BUFFER = Array.new
+mutex = Mutex.new
 
 def load_config()
 	
@@ -29,46 +27,52 @@ def load_config()
 	$pass = globalConf['pass']
 	$vhost = globalConf['vhost']
 	$max_buffer = globalConf['max_buffer'].to_i
-	puts $max_buffer
 end 
 
-def write_amqp()
+load_config()
 
-	Syslog.open("Rlogix", Syslog::LOG_PID, Syslog::LOG_DAEMON | Syslog::LOG_LOCAL3)
-	
-	ARGF.each do |line|
-		
-		BUFFER << line
+collector = Thread.new do
+	mutex.synchronize do
 
+	    	ARGF.each do |line|
+	            BUFFER << line
+	    	end
+
+	end
+
+end
+
+sender = Thread.new do
+	mutex.synchronize do
+
+
+		#if ! a.empty?
+					
 		if BUFFER.length >= $max_buffer
-
+		
 			conn = Bunny.new(:user => $user, :pass => $pass, :host => $server, :logging => true)
 			#, :logfile => "/tmp/bunny.log")
 			conn.start
 			q = conn.queue($queue)
-			
+		
 			BUFFER.each do |msg|
 				q.publish(msg, :persistent => false)
-				puts "##################################################"
+				puts "msg"
 			end
 			conn.stop
-
 			BUFFER.clear
+
 		else
-			puts "PUTS TIME ################################"
+			puts "Não atingiu #{$max_buffer} - #{BUFFER.length}"
 		end
 
-		BUFFER.each do |pos|
-			puts pos
-		end
-		
-		w = BUFFER.length
-		puts w
-
+ 		
+  		
 	end
-
-	Syslog.close()
 end
+mutex.lock
 
-load_config()
-write_amqp()
+#r = collector.alive?
+#t = sender.alive?
+#puts r
+#puts t
